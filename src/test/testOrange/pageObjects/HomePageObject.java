035f7b61ef470651c0b2ea1b9902652cbdf9c785
Adrian <adrian@AdrianPCGamer>
2022-05-09 22:31:07 -0500

    Commit new branch

--- src/test/testOrange/pageObjects/HomePageObject.java
+++ src/test/testOrange/pageObjects/HomePageObject.java
@@ -4,5 +4,6 @@ public class HomePageObject {
 	 public static final String USER_TXBOX = "txtUsername";
 	 public static final String PASSWORD_TXBOX = "txtPassword";
 	 public static final String LOGIN_BUTTON = "btnLogin";
+	 public static final String NEW = "";
 	 
 }
